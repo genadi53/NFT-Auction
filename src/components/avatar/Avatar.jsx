1b7c38fb13ef20b57e33dbdc25f2a2812b3346d5
genadi53 <kotka5353@gmail.com>
2021-10-16 11:22:04 +0300

    feat: add avatar component

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,51 @@
+// import VerifiedIcon from "@mui/icons-material/Verified";
+// import Badge from "@mui/material/Badge";
+import styles from "./Avatar.module.scss";
+import classNames from "classnames";
+
+export default function Avatar({
+  url = "./images/avatar.png",
+  size = 90,
+  verified = false,
+}) {
+  return (
+    <div
+      className={classNames(styles.avatar)}
+      style={{ width: size, height: size }}
+    >
+      <img
+        className={classNames(styles.image)}
+        src={url}
+        // style={{ width: `${size}px; height: ${size}px` }}
+        style={{ width: size, height: size }}
+      />
+      {verified ? (
+        // <VerifiedIcon
+        //   className={classNames(styles.badge)}
+        //   color="success"
+        //   role="img"
+        // />
+        <img
+          className={classNames(styles.badge)}
+          alt="verified"
+          style={{ width: size / 3, height: size / 3 }}
+          src={"/images/verified.svg"}
+        />
+      ) : null}
+    </div>
+    // <div
+    //   className={classNames(styles.avatar)}
+    //   style={{ width: `${size}px; height: ${size}px` }}
+    // >
+    //   <Badge
+    //     overlap="circular"
+    //     anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+    //     badgeContent={
+    //       verified ? <VerifiedIcon color="success" role="img" /> : null
+    //     }
+    //   >
+    //     <img className={classNames(styles.image)} src={url} alt="avatar" />
+    //   </Badge>
+    // </div>
+  );
+}
