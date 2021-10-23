f86b4d228adc7afc949aca2fd44fd69bba2df904
genadi53 <kotka5353@gmail.com>
2021-10-23 13:20:26 +0300

    refactor footer and delete card copy

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,16 +4,85 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
 
+import Card from "../../src/components/card/Card";
+import Footer from "../../src/components/footer/Footer";
+import User from "../../src/components/user/User";
+import Trending from "../../src/components/trending/Trending";
+import Header from "../../src/components/header/Header";
+
+const userProps = {
+  name: "Okay",
+  info: "boomer",
+  avatar: "/images/avatar.png",
+  verified: true,
+  size: 55,
+};
+
+const cardProps = {
+  name: "Clock",
+  likes: 3500,
+  mediaUrl: "/images/nft.jpg",
+  user: {
+    avatarUrl: "/images/avatar.png",
+    verified: true,
+  },
+  price: "11.9",
+  currency: "ETH",
+};
+
+const trendingProps = {
+  cards: [
+    {
+      name: "Clock",
+      user: { avatarUrl: "images/avatar.png", verified: true },
+      mediaUrl: "images/nft.jpg",
+      price: 200,
+      currency: "BTC",
+    },
+    {
+      name: "DOGE",
+      user: { avatarUrl: "images/avatar.png", verified: true },
+      mediaUrl: "images/nft.jpg",
+      price: 200,
+      currency: "BTC",
+    },
+    {
+      name: "BTC",
+      user: { avatarUrl: "images/avatar.png", verified: true },
+      mediaUrl: "images/nft.jpg",
+      price: 100,
+      currency: "BTC",
+    },
+    {
+      name: "Litecoin",
+      user: { avatarUrl: "images/avatar.png", verified: true },
+      mediaUrl: "images/nft.jpg",
+      price: 300,
+      currency: "BTC",
+    },
+  ],
+};
+
 export default function About() {
   return (
-    <Container maxWidth="sm">
-      <Grid container justifyContent="center">
-        <Grid item>
-          <Button variant="contained" component={Link} noLinkStyle href="/">
-            🏠 Home
-          </Button>
+    <>
+      <Header />
+      <Trending cards={trendingProps.cards} />
+      <br />
+      <Card {...cardProps} />
+      <br />
+      <Card {...cardProps} />
+      <br />
+      <Container maxWidth="sm">
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Button variant="contained" component={Link} noLinkStyle href="/">
+              🏠 Home
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Footer muted={"muted"} />
+    </>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -31,21 +31,4 @@ export default function Footer({ muted }) {
       </Grid>
     </Container>
   );
-
-  //   return (
-  //     <Container className={classNames(styles.container)} maxWidth="xl">
-  //       <Grid container spacing={2}>
-  //         <Grid item xs={4}>
-  //           <Logo {...muted} />
-  //         </Grid>
-  //         <Grid item xs={4}>
-  //           <p>Copyright</p>
-  //         </Grid>
-  //         <Grid item xs={4}>
-  //           <Button className={classNames(styles.policy)}>Privacy Policy</Button>
-  //           <Button className={classNames(styles.policy)}>Cookie Policy</Button>
-  //         </Grid>
-  //       </Grid>
-  //     </Container>
-  //   );
 }
