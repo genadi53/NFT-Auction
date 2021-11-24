fcb5ccdffafaf4148519a364d6d6096985dc1f67
genadi53 <kotka5353@gmail.com>
2021-11-24 15:47:29 +0200

    add hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import classNames from "classnames";
+import styles from "./Hero.module.scss";
+
+export default function Hero({ text }) {
+  return (
+    <div className={classNames(styles.hero)}>
+      <Typography variant={"h1"} className={classNames(styles.text)}>
+        {text}
+      </Typography>
+    </div>
+  );
+}
