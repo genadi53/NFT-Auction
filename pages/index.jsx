1de502c3c4bac4f63bb30ee94300b92480a7ad1b
genadi53 <kotka5353@gmail.com>
2021-11-24 15:47:06 +0200

    feat: finished product page

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,6 +37,38 @@ const howProps = {
   link: "https://google.com",
 };
 
+const productContainerProps = {
+  name: "Ergonomic Concrete Tuna",
+  owner: {
+    username: "Justen_King18",
+    verified: true,
+    avatar: {
+      url: "https://nft-auction.herokuapp.com/uploads/thumbnail_0x7d9debcf75a71bbb5c533804c9845d313fe3f6aa_ec98dd79b9.jpg",
+    },
+  },
+  price: 20,
+  currency: "ETH",
+  likes: 25,
+  auction_end: "2022-09-02T20:43:19.149Z",
+  details: "asdasdads",
+  source: {
+    url: "https://nft-auction.herokuapp.com/uploads/thumbnail_0x7d9debcf75a71bbb5c533804c9845d313fe3f6aa_ec98dd79b9.jpg",
+  },
+  bids: [
+    {
+      user: {
+        info: 20,
+        name: "John",
+        verified: true,
+        avatar:
+          "https://nft-auction.herokuapp.com/uploads/thumbnail_0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg",
+      },
+      date: "2021-10-22T08:29:23.382Z",
+      amount: 20,
+    },
+  ],
+};
+
 export default function Index() {
   const [featuredCards, setFeaturedCards] = useState([]);
   const [topCollectors, setTopCollectors] = useState([]);
---
+++ pages/product/[id]/index.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import dataNfts from "../../../data/nfts.json";
+import { useRouter } from "next/router";
+import Header from "../../../src/components/header/Header.jsx";
+import ProductContainer from "../../../src/components/product/ProductContainer.jsx";
+import Footer from "../../../src/components/footer/Footer.jsx";
+
+export default function Index() {
+  const router = useRouter();
+  const [nft, seNft] = useState(null);
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { id } = router.query;
+      seNft(dataNfts.find((nft) => nft.id == id));
+    }
+  }, [router]);
+
+  const mockBids = [
+    {
+      user: { avatar: "/images/avatar.png", name: "hrisi", verified: true },
+      amount: 30,
+      date: "2021-10-22T08:29:23.382Z",
+    },
+    {
+      user: { avatar: "/images/avatar.png", name: "maxi", verified: true },
+      amount: 1000,
+      date: "2021-10-22T08:29:23.382Z",
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      {nft && (
+        <ProductContainer
+          name={nft.name}
+          owner={nft.owner}
+          price={nft.price}
+          currency={nft.currency}
+          likes={nft.likes}
+          auction_end={nft.auction_end}
+          details={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur, metus lacinia tempus mollis, risus lectus hendrerit ante, non vulputate neque elit quis urna."
+          }
+          bids={mockBids}
+          source={nft.source}
+          isLive={true}
+          buyAmount={nft.price + 5}
+          bidAmount={nft.price}
+          onBuy={() => {}}
+          onBid={() => {}}
+          onTimeEnd={() => {}}
+        ></ProductContainer>
+      )}
+      <Footer />
+    </div>
+  );
+}
