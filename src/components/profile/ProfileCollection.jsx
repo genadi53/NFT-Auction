3a313aafec0040f6c1435e2fa97388da4bd998e5
genadi53 <kotka5353@gmail.com>
2021-11-26 13:53:17 +0200

    feat: add profile Collection component

---
+++ src/components/profile/ProfileCollection.jsx
@@ -0,0 +1,41 @@
+import classNames from "classnames";
+import styles from "./ProfileCollection.module.scss";
+import { Container, Grid, Typography } from "@mui/material/";
+import ProfileCollectionFilters from "./ProfileCollectionFilters.jsx";
+import Card from "../card/Card.jsx";
+
+export default function ProfileCollection({ user, filters, items = [] }) {
+  return (
+    <div className={classNames(styles["profile-collection"])}>
+      <Container>
+        <Grid container sx={{ alignItems: "baseline" }}>
+          <Grid item xs={3}>
+            <Typography variant={"h3"}>Collection</Typography>
+          </Grid>
+          <Grid item xs={9}>
+            <ProfileCollectionFilters filters={filters} />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} sx={{ marginTop: "50px" }}>
+          {items.map((item) => {
+            return (
+              <Grid item xs={3} key={item.id}>
+                <Card
+                  user={{
+                    verified: user.verified,
+                    avatarUrl: user.avatar,
+                  }}
+                  name={item.name}
+                  price={item.price}
+                  likes={item.likes}
+                  currency={item.currency}
+                  mediaUrl={item.source.url}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
---
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import {
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+  Stack,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+import { Search } from "@mui/icons-material";
+import classNames from "classnames";
+import styles from "./ProfileCollectionFilters.module.scss";
+
+export default function ProfileCollectionFilters({ filters }) {
+  const [sortFilter, setSortFilter] = useState("");
+  const [priceFilter, setPriceFilter] = useState("");
+
+  return (
+    <div className={classNames(styles["profile-collection-filters"])}>
+      <Stack
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        spacing={2}
+      >
+        <FormControl sx={{ width: "220px" }}>
+          <InputLabel id="sort-by-label">Sort by</InputLabel>
+          <Select
+            labelId="sort-by-label"
+            sx={{ minWidth: "170px" }}
+            value={sortFilter}
+            onChange={(event) => {
+              setSortFilter(event.target.value);
+            }}
+          >
+            {filters.sort.map((filter, idx) => {
+              return (
+                <MenuItem value={filter.value} key={idx}>
+                  {filter.label}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: "220px" }}>
+          <InputLabel id="price-range-label">Price range</InputLabel>
+          <Select
+            labelId="price-range-label"
+            sx={{ minWidth: "170px" }}
+            value={priceFilter}
+            onChange={(event) => {
+              setPriceFilter(event.target.value);
+            }}
+          >
+            {filters.price.map((price, idx) => {
+              return (
+                <MenuItem key={idx} value={price.value}>
+                  {price.label}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: "340px" }}>
+          <TextField
+            fullWidth
+            className={classNames(styles.search)}
+            InputProps={{
+              disableUnderline: true,
+              startAdornment: (
+                <InputAdornment
+                  position="start"
+                  color="secondary"
+                  variant="standard"
+                >
+                  <Search className={styles["search-icon"]} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </FormControl>
+      </Stack>
+    </div>
+  );
+}
