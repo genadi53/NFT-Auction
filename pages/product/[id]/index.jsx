850b651ae5460d6521558037ba80dfe1af07e702
genadi53 <kotka5353@gmail.com>
2021-11-26 12:30:55 +0200

    feat: add how page

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -5,7 +5,7 @@ import Header from "../../../src/components/header/Header.jsx";
 import ProductContainer from "../../../src/components/product/ProductContainer.jsx";
 import Footer from "../../../src/components/footer/Footer.jsx";
 
-export default function Index() {
+export default function Product() {
   const router = useRouter();
   const [nft, seNft] = useState(null);
 
